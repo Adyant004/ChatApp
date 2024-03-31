3f3618f1c7c4f19f7e01a75efb02fd93e49dccb3
Adyant <adyantajay@gmail.com>
2024-03-31 15:31:53 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { app,server } = require('./Socket/socket')
+const { app, server } = require('./Socket/socket')
 const authRoutes = require('./Routes/authRoutes')
 const messageRoutes = require('./Routes/messageRoutes')
 const userRoutes = require('./Routes/userRoutes')
@@ -7,6 +7,7 @@ const connectToMongo = require('./db/connect');
 require('dotenv').config()
 const cookieParser = require('cookie-parser');
 const { v2 } = require('cloudinary');
+const path = require('path');
 
 const port = process.env.PORT || 5000;
 
@@ -20,11 +21,17 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/auth',authRoutes);
-app.use('/api/message',messageRoutes);
-app.use('/api/users',userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
     connectToMongo(process.env.MONGO_DB_URI);
     console.log(`Server is listening on port ${port}...`)
-})
+});
